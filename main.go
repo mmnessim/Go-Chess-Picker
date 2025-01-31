b4af34f56e1269c3ecff72dfb62967a22130dbe4
Mounir Nessim <mmnessim@gmail.com>
2025-01-30 20:16:27 -0500

    remove CLI loop function

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 
-	"go-chess/game"
 	"go-chess/handlers"
-	"go-chess/user"
 )
 
 func main() {
@@ -20,28 +15,4 @@ func main() {
 	http.HandleFunc("/", handlers.Index)
 	http.HandleFunc("/game", handlers.Game)
 	http.ListenAndServe(":8080", nil)
-}
-
-func PickUser() {
-	fmt.Println("Enter Chess.com Username:")
-
-	scanner := bufio.NewReader(os.Stdin)
-
-	username, _ := scanner.ReadString('\n')
-	u := user.New(strings.TrimSpace(username))
-	u.GetArchives()
-
-	for {
-		fmt.Println("Get random game?y/n")
-		answer, _ := scanner.ReadString('\n')
-		if strings.TrimSpace(answer) == "y" {
-			randomGame := game.GetRandomGame(&u)
-			randomGame.Summary()
-		} else if strings.TrimSpace(answer) == "n" {
-			break
-		} else {
-			continue
-		}
-	}
-
 }
