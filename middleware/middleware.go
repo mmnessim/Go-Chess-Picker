5486e9e891f7aea9ace4edff455e7cce6b833a6c
Mounir Nessim <mmnessim@gmail.com>
2025-02-02 14:45:04 -0500

    add timing to logging middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,11 +3,13 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func Logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.URL.Path)
+		now := time.Now()
+		log.Println(time.Since(now), r.Method, r.URL.Path)
 		f(w, r)
 	}
 }
