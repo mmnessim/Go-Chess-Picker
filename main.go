5fa2f8fae945bed5ed29adf85065681ac867d8fd
Mounir Nessim <mmnessim@gmail.com>
2025-02-02 06:46:20 -0500

    basic logging middleware

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"net/http"
 
 	"go-chess/handlers"
+	"go-chess/middleware"
 )
 
 func main() {
+	index := middleware.Logging(handlers.Index)
+	game := middleware.Logging(handlers.Game)
 
 	fmt.Println("Listening on port 8080")
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", handlers.Index)
-	http.HandleFunc("/game", handlers.Game)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/game", game)
 	http.ListenAndServe(":8080", nil)
 }
