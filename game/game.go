0bdd36f7568647c793daf9bb5a42a95519ab7e02
Mounir Nessim <mmnessim@gmail.com>
2025-02-03 06:04:44 -0500

    remove linked list stuff :(

--- game/game.go
+++ game/game.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -30,40 +29,4 @@ type White struct {
 func (g *Game) Summary() {
 	fmt.Println("Black:", g.Black, "White", g.White)
 	fmt.Println("Go Analyze:", g.Url)
-}
-
-type GameNode struct {
-	Game  Game
-	Index int
-	Next  *GameNode
-}
-
-type GameList struct {
-	Head   *GameNode
-	Length int
-}
-
-func (gl *GameList) InsertAtHead(game Game) {
-	temp1 := &GameNode{game, gl.Length, nil}
-
-	if gl.Head == nil {
-		gl.Head = temp1
-	} else {
-		temp2 := gl.Head
-		gl.Head = temp1
-		temp1.Next = temp2
-	}
-	gl.Length += 1
-}
-
-func (gl *GameList) GetFromIndex(index int) (*Game, error) {
-	cur := gl.Head
-	for {
-		if cur.Index == index {
-			return &cur.Game, nil
-		} else if cur.Next == nil {
-			return nil, errors.New("index outside of bounds")
-		}
-		cur = cur.Next
-	}
 }
