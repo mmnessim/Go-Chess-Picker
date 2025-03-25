636c851ab84a328b83a9997e2d14a7932ab56eae
Mounir Nessim <mmnessim@gmail.com>
2025-03-25 06:36:25 -0400

    add host to logging

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -10,6 +10,7 @@ func Logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
 		log.Println(time.Since(now), r.Method, r.URL.Path)
+		log.Println("From", r.Host)
 		f(w, r)
 	}
 }
