86eefd3e082202abecf7b071ff421ca4d26aef51
Mounir Nessim <mmnessim@gmail.com>
2025-03-25 06:39:18 -0400

    fix host logging

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -10,7 +10,7 @@ func Logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
 		log.Println(time.Since(now), r.Method, r.URL.Path)
-		log.Println("From", r.Host)
+		log.Println("From", r.URL.Host)
 		f(w, r)
 	}
 }
