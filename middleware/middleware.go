8844fe841f680e4a29c5493d749da6e4d2c3ba66
Mounir Nessim <mmnessim@gmail.com>
2025-03-26 21:08:02 -0400

    remove unnecessary line

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -10,7 +10,6 @@ func Logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
 		log.Println(time.Since(now), r.Method, r.URL.Path)
-		log.Println("\n", r.Header["Referer"])
 		f(w, r)
 	}
 }
